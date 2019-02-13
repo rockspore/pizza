be6dd19d26c5bfc6659ef5776380ce5dd935fbe5
Archelangelo <medycc@gmail.com>
2019-02-12 21:23:23 -0500

    Fixed judge.cpp version 1

--- src/judge.cpp
+++ src/judge.cpp
@@ -13,6 +13,14 @@
 
 using namespace std;
 
+bool overlap1d(int a, int b, int c, int d) {
+    return !((a<c && a<d && b<c && b<d) || (a>c && a>d && b>c && b>d));
+}
+
+bool overlap(vector<int>& a, vector<int>& b) {
+    return overlap1d(a[0], a[1], b[0], b[1]) && overlap1d(a[2], a[3], b[2], b[3]);
+}
+
 int Pizza::pizzaJudge() {
     bool f = true;
     int area = 0;
@@ -21,9 +29,13 @@ int Pizza::pizzaJudge() {
         int r1 = min(ans[i][0], ans[i][2]), r2 = max(ans[i][0], ans[i][2]),
             c1 = min(ans[i][1], ans[i][3]), c2 = max(ans[i][1], ans[i][3]),
             thisarea = (r2 - r1 + 1) * (c2 - c1 + 1),
-            num_ing = accum_tab[r1][c1] + accum_tab[r2+1][c2+1] - accum_tab[r1][c2+1] - accum_tab[r2+1][c1];
+            num_toma = accum_tab[r1][c1].toma + accum_tab[r2+1][c2+1].toma
+                    - accum_tab[r1][c2+1].toma - accum_tab[r2+1][c1].toma,
+            num_mush = accum_tab[r1][c1].mush + accum_tab[r2+1][c2+1].mush
+                    - accum_tab[r1][c2+1].mush - accum_tab[r2+1][c1].mush,
+            num_ing = min(num_toma, num_mush);
 
-        if (thisarea > max_cells || num_ing < min_ing) {
+        if (thisarea > max_ncells || num_ing < min_ing) {
             f = false;
             break;
         }
@@ -45,10 +57,3 @@ int Pizza::pizzaJudge() {
     }
 }
 
-bool overlap(vector<int>& a, vector<int>& b) {
-    return overlap1d(a[0], a[1], b[0], b[1]) && overlap1d(a[2], a[3], b[2], b[3]);
-}
-
-bool overlap1d(int a, int b, int c, int d) {
-    return !((a<c && a<d && b<c && b<d) || (a>c && a>d && b>c && b>d));
-}
