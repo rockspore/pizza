51ff998bef5ddf481efdbca4e8366ef9a2c9a3b1
Archelangelo <medycc@gmail.com>
2019-02-15 01:55:40 -0500

    Changed directory for allslices_test.cpp. allslices_test.cpp tested. Compilation no problem.

--- test/allslices_test.cpp
+++ test/allslices_test.cpp
@@ -1,27 +1,26 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <cstring>
 
 using namespace std;
 
-int main(int argc, char* argv[]) {
+void findSlices(char* filename, vector<vector<int>>& slices) {
     fstream fs;
-    if (argc > 0)
-        fs.open(argv[0]);
-    else
-        fs.open("../data/a_example.in");
-
+    fs.open(filename, ios_base::in);
     int n, m, min_ing, max_cell;
-    vector<vector<int>> acct(n+1, vector<int>(m+1, 0));
-    vector<vector<int>> accm(n+1, vector<int>(m+1, 0));
-
+    
+    cout << "Reading file: " << filename << endl;
     if (fs.is_open()) {
         fs >> n >> m >> min_ing >> max_cell;
+        vector<vector<int>> acct(n+1, vector<int>(m+1, 0));
+        vector<vector<int>> accm(n+1, vector<int>(m+1, 0));
+
         std::string temp_str;
         getline(fs, temp_str);
         for (int i = 1; i <= n; i++) {
             getline(fs, temp_str);
-            for (int j = 1; i <= m; j++) {
+            for (int j = 1; j <= m; j++) {
                 acct[i][j] = acct[i-1][j] + acct[i][j-1] - acct[i-1][j-1];
                 accm[i][j] = accm[i-1][j] + accm[i][j-1] - accm[i-1][j-1];
                 if (temp_str[j-1] == 'T') {
@@ -32,7 +31,11 @@ int main(int argc, char* argv[]) {
             }
         }
         fs.close();
-        
+       
+        cout << "Pizza size:" << n << " * " << m << endl;
+        cout << "Minimum ingredient: " << min_ing << endl;
+        cout << "Maximum slice: " << max_cell << endl;
+
         // find rectangle shapes
         vector<vector<int>> shapes;
         for (int l = 1; l <= max_cell; l++) {
@@ -41,23 +44,28 @@ int main(int argc, char* argv[]) {
                     shapes.push_back(vector<int>(2, 0));
                     shapes[shapes.size()-1][0] = w;
                     shapes[shapes.size()-1][1] = l;
+                    // cout << "Adding shape: " << w << " * " << l << endl;
                 }
             }
         }
 
-        vector<vector<int>> rects;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 for (int k = 0; k < shapes.size(); k++) {
                     int i2 = i + shapes[k][0], j2 = j + shapes[k][1];
+                    if (i2 > n || j2 > m) continue;
+                    // cout << "Slice [" << i << "," << j << "]->[" << i2-1 << "," << j2-1 << "]" << endl;
+                    // cout << "Tomatos: " << acct[i2][j2] + acct[i][j] - acct[i2][j] - acct[i][j2] << endl;
+                    // cout << "Mushrooms: " << accm[i2][j2] + accm[i][j] - accm[i2][j] - accm[i][j2] << endl;
+                    // cout << "Thanks for participating" << endl;
                     if (acct[i2][j2] + acct[i][j] - acct[i2][j] - acct[i][j2] >= min_ing && 
                         accm[i2][j2] + accm[i][j] - accm[i2][j] - accm[i][j2] >= min_ing) {
-                        vector<int> temp_rect(4, 0);
-                        temp_rect[0] = i;
-                        temp_rect[1] = j;
-                        temp_rect[2] = i2-1;
-                        temp_rect[3] = j2-1;
-                        rects.push_back(temp_rect);
+                        vector<int> temp_slice(4, 0);
+                        temp_slice[0] = i;
+                        temp_slice[1] = j;
+                        temp_slice[2] = i2-1;
+                        temp_slice[3] = j2-1;
+                        slices.push_back(temp_slice);
                     }
                 }
             }
@@ -68,5 +76,14 @@ int main(int argc, char* argv[]) {
         cout << "Error opening the file\n";
     }
 
-    return 0;
+}
+
+int main(int argc, char* argv[]){
+    char *filename = new char(100);
+    if (argc > 1) strcpy(filename, argv[1]);
+    else strcpy(filename, "../data/a_example.in");
+
+    vector<vector<int>> slices;
+    findSlices(argv[1], slices);
+    cout << slices.size() << endl;
 }
